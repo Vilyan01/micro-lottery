bfe4e2e6a162b979c3d3dc88e095d2c023f1e179
Brian Heller <bheller2010@live.com>
2017-01-02 12:40:19 -0800

    Removed some logging

--- app/assets/javascripts/games/GameCtrl.js
+++ app/assets/javascripts/games/GameCtrl.js
@@ -13,10 +13,6 @@ angular.module('lottery')
       };
 
       $scope.addGame = function() {
-        console.log("Adding a game");
-        console.log($scope.title);
-        console.log($scope.max);
-        console.log($scope.buy_in);
         if(!$scope.title || $scope.title === '') {return;}
         if(!$scope.max || $scope.max < 0) {return;}
         if(!$scope.buy_in || $scope.buy_in === '') {return;}
